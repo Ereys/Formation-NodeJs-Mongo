6627ed59ad6d6248c658bef0659e18318c25ba89
Pierre Ribollet <46523223+Ereys@users.noreply.github.com>
2023-01-09 15:12:16 +0100

    add nodemon + dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 const my_string = 'Hello World!' // Valide en JS et TS
 const my_typed_string: string = 'Hello World!' // Valide en TS
 
-console.log(my_string);
+type personne = {
+    lastname: any
+    firstname: any
+}
+
+
+const user:personne =  {
+    lastname : process.env.LASTNAME,
+    firstname: process.env.FIRSTNAME
+}
+
+
+const {lastname, firstname} = user;
+console.log(`Bonjour ${firstname} ${lastname}, comment allez-vous ?`);
+
+console.log(process.env); // Print l'ensemble des %VAR%
+
